045c8221399cee456650facc167d1e24d3f8160b
XiaoLux <dcarlos.mx@gmail.com>
2023-06-29 19:08:45 -0600

    handleSubmit con refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import "./App.css";
 import { useMovies } from "./hooks/useMovies";
 import { Movies } from "./components/Movies";
+import { useRef } from "react";
 
-// console.log()
 // https://www.omdbapi.com/?s=Avengers&apikey=${import.meta.env.VITE_OMDBAPI_KEY}
 
-function App() {
-  const { movies: mappedMovies } = useMovies();
+function App() {  
+  const { movies } = useMovies();
+  const inputRef = useRef()
+
+  /* Esto esta bien, pero se puede recuperar la información
+  /  del formulario desde la misma llamada on submit
+  /  para asi evitar estar crenado muchos "useRefs" */
+  const handleSubmit = () => {
+    event.preventDefault()
+    const value = inputRef.current.value
+    console.log(value)
+  }
 
   return (
     <div className="page">
       <h1>Hola mundo</h1>
       <header>
-        <form action="" className="form">
-          <input placeholder="Avengers, Forrest Gump..." type="text" />
+        <form action="" className="form" onSubmit={handleSubmit}>
+          <input ref={inputRef} placeholder="Avengers, Forrest Gump..." type="text" />
           <button type="submit">Buscar</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   );
