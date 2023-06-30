5da050aa20aeb46707759184e788f0fbe32d7827
XiaoLux <dcarlos.mx@gmail.com>
2023-06-30 17:59:28 -0600

    useQuery prevent check first render

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,23 +1,28 @@
-
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export function useQuery() {
-    const [query, updateQuery] = useState("")  
-    const [error, setError] = useState(null)
-  
-    useEffect(() => {
-      if (query === '') {
-        setError("Se requiere el título de una pelicula")
-        return
-      }
-  
-      if (query.length < 3) {
-        setError("La búsqueda debe tener la menos 3 caracteres")
-        return
-      }
-  
-      setError(null)
-    }, [query])
-  
-    return {query, updateQuery, error}
-  }
+  const [query, updateQuery] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = query === "";
+      return
+    }
+
+    if (query === "") {
+      setError("Se requiere el título de una pelicula");
+      return;
+    }
+
+    if (query.length < 3) {
+      setError("La búsqueda debe tener la menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [query]);
+
+  return { query, updateQuery, error };
+}
