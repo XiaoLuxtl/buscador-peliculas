d0e46901ce4aafa6e0a9a81858c2c3c59be02ec7
XiaoLux <dcarlos.mx@gmail.com>
2023-07-04 16:05:20 -0600

    add sort nad some optimizations

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,15 +1,15 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useMemo, useCallback } from "react";
 import { searchMovies } from "../services/movies";
 
 // https://www.omdbapi.com/?s=Avengers&apikey=${import.meta.env.VITE_OMDBAPI_KEY}
 
-export function useMovies({ query }) {
+export function useMovies({ query, sort }) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const previousQuery = useRef(query);
 
-  const getMovies = async () => {
+  const getMovies = useCallback(async ({ query }) => {
     if (query === previousQuery.current) return;
     try {
       setLoading(true);
@@ -22,7 +22,15 @@ export function useMovies({ query }) {
     } finally {
       setLoading(false);
     }
-  };
+  }, []);
 
-  return { movies, loading, error, getMovies };
+  // Evitar el re-renderizado al escribir nuevos términos
+  // ya que react ejecuta de nuevo el cuerpo de la función
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
+  }, [sort, movies]);
+
+  return { movies: sortedMovies, loading, error, getMovies };
 }
